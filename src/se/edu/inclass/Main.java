3ab392fed8ac30b2eaa353db8d87f7b6d2d841ec
APANA49 <aungphonenaing57@gmail.com>
2023-02-17 17:06:03 +0800

    Add methods to count deadlines using streams

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -25,6 +25,13 @@ public class Main {
 
     }
 
+    private static int countDeadlinesUsingStream(ArrayList<Task> tasks) {
+        int count = (int) tasks.stream()
+                .filter(t -> t instanceof Deadline)
+                .count(); // take note count returns a long so need to cast to int
+
+        return count;
+    }
     private static int countDeadlines(ArrayList<Task> tasksData) {
         int count = 0;
         for (Task t : tasksData) {
